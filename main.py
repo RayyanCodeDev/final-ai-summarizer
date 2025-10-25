3392a3a15ff56ac031f6de19571256a7756e6c86
RayyanCodeDev <tcs431756@gmail.com>
2025-10-25 09:00:37 +0000

    Change port to 8000

--- main.py
+++ main.py
@@ -242,6 +242,6 @@
     # --- Flask Run ---
     if __name__ == "__main__":
         keep_alive()   # <--- Start the self-pinger
-        port = int(os.environ.get("PORT", 3000))
+        port = int(os.environ.get("PORT", 8000))
         print(f"Starting server on 0.0.0.0:{port}")
         app.run(host="0.0.0.0", port=port, debug=True)
